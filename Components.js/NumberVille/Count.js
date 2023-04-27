d6a8dddd4d708db5050159d6ad28b79b16d3370a
Joe <spizjo01@gmail.com>
2023-04-27 15:33:09 -0600

    counting game started. built about 75%. items go grey when clicked and when all are clicked then next number rotates. need to change images and styling based on number

--- Components.js/NumberVille/Count.js
+++ Components.js/NumberVille/Count.js
@@ -1,9 +1,15 @@
-import {View, Text } from 'react-native';
+import {View, Text, Image, TouchableWithoutFeedback } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import {Audio} from 'expo-av'   
 
 function Count({navigation}) {
+    const [number, setNumber] = useState(2)
+    const [tally, setTally]= useState(0)
+    const [pressed, setPressed] = useState([]);
+   
+    const image = "../../assets/trucks/truck1.png"
+
     useEffect(()=>{
         async function welcome() {
           const { sound } = await Audio.Sound.createAsync(
@@ -12,14 +18,40 @@ function Count({navigation}) {
           await sound.playAsync();
         }
         welcome()
-      })
+      },[])
+      
+      const block = []
+   
+      const playerCounts = (i)=>{
+       setPressed([...pressed, i])
+        setTally(tally+1)
+        if((tally+1)===number){
+            setNumber(number+1)
+            setTally(0)
+            setPressed([])
+        }}
+      for (let i = 0; i < number; i++) {
+      block.push(<View key={i} style={pressed.includes(i) ? styles.buttonPressed : undefined}><TouchableWithoutFeedback 
+       
+      onPress={pressed.includes(i) ? null: ()=>playerCounts(i) }><Text><Image style={styles.picture} source={require(image)}/>
+          {number}</Text></TouchableWithoutFeedback></View>);
+      }
+
 const goHome= ()=>{
     navigation.navigate(`Numbers`)
 }
+
   return (
     <View style={styles.container}>
         <Text style={styles.title}>Count</Text>
         <Text style={styles.title2}>How many do you see?</Text>
+<View style={styles.countBoxContainer}>
+        <View style={styles.countBox}>
+
+       {block}
+      
+        </View>
+        </View>
         
         <Text style={styles.button} 
         onPress={goHome}
@@ -49,7 +81,29 @@ const styles= EStyleSheet.create({
       color: "#00FC00",
       fontWeight: 'bold'
     },
-
+    countBoxContainer:{
+        flex: 1,
+    justifyContent: 'center', // aligns children vertically
+    alignItems: 'center', // aligns children horizontally
+    },
+    countBox:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 15,
+        borderStyle: "dashed",
+        elevation: 15,
+        height: "60%",
+        width: "80%",
+        backgroundColor: "grey"
+    },
+    picture:{
+          width: 50,
+          height: 50,
+          resizeMethod: "contain",
+    },
+    buttonPressed: {
+       opacity: .3
+      },
     button: {
         borderRadius: 10,
         borderWidth: 5,
