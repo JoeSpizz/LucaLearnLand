f77cbaf376ecc72fd67545f29f0d8c6121e30199
Joe <spizjo01@gmail.com>
2023-04-28 16:31:17 -0600

    counting game has numbers appear as you click in order. styling and success vs failure animations needed to finish

--- Components.js/NumberVille/Count.js
+++ Components.js/NumberVille/Count.js
@@ -4,11 +4,20 @@ import {useEffect, useState} from 'react'
 import {Audio} from 'expo-av'   
 
 function Count({navigation}) {
-    const [number, setNumber] = useState(2)
+    const [number, setNumber] = useState(1)
     const [tally, setTally]= useState(0)
     const [pressed, setPressed] = useState([]);
+    const images = [
+      require('../../assets/trucks/truck1.png'),
+      require('../../assets/trucks/truck-7.png'),
+      require('../../assets/trucks/truck6.png'),
+      require('../../assets/trucks/truck1.png'),
+      require('../../assets/trucks/truck-7.png'),
+      require('../../assets/trucks/truck6.png'),
+    ];
+    const [counters, setCounters] = useState(Array(number).fill(null));
    
-    const image = "../../assets/trucks/truck1.png"
+
 
     useEffect(()=>{
         async function welcome() {
@@ -21,20 +30,39 @@ function Count({navigation}) {
       },[])
       
       const block = []
-   
+  
       const playerCounts = (i)=>{
-       setPressed([...pressed, i])
-        setTally(tally+1)
+        if (!counters[i]) {
+          const newCounters = [...counters];
+          newCounters[i] = tally + 1;
+          setCounters(newCounters);
+          setTally(tally+1);
+          
+          setPressed([...pressed, i])
         if((tally+1)===number){
             setNumber(number+1)
             setTally(0)
             setPressed([])
-        }}
+            setCounters(Array(number+1).fill(null));
+        }
+      }
+      }
+      
+
       for (let i = 0; i < number; i++) {
-      block.push(<View key={i} style={pressed.includes(i) ? styles.buttonPressed : undefined}><TouchableWithoutFeedback 
-       
-      onPress={pressed.includes(i) ? null: ()=>playerCounts(i) }><Text><Image style={styles.picture} source={require(image)}/>
-          {number}</Text></TouchableWithoutFeedback></View>);
+      block.push(
+      <View key={i} >
+        <TouchableWithoutFeedback onPress={pressed.includes(i) ? null: ()=>playerCounts(i) }>
+          <Text style={pressed.includes(i) ? styles.buttonPressed : undefined}>
+              <Image style={styles.picture} source={images[number-1]}/>  
+         </Text>
+         
+         
+          </TouchableWithoutFeedback>
+          <View>
+         <Text style={pressed.includes(i)? styles.visible : styles.invisible}>{counters[i]}</Text>
+         </View>
+          </View>);
       }
 
 const goHome= ()=>{
@@ -43,19 +71,23 @@ const goHome= ()=>{
 
   return (
     <View style={styles.container}>
-        <Text style={styles.title}>Count</Text>
-        <Text style={styles.title2}>How many do you see?</Text>
-<View style={styles.countBoxContainer}>
+        <Text style={styles.title}>
+          Count
+          </Text>
+        <Text style={styles.title2}>
+          How many do you see?
+        </Text>
+        
+        <View style={styles.countBoxContainer}>
         <View style={styles.countBox}>
-
        {block}
-      
         </View>
         </View>
         
         <Text style={styles.button} 
-        onPress={goHome}
-        > Back to the NumberVille</Text>
+        onPress={goHome}> 
+        Back to the NumberVille
+        </Text>
     </View>
   )
 }
@@ -103,6 +135,14 @@ const styles= EStyleSheet.create({
     },
     buttonPressed: {
        opacity: .3
+      },
+      visible:{
+        color: 'rgba(255, 0, 255, 1.0)',
+        fontSize: '1.5rem',
+        fontWeight: 'bold'
+      },
+      invisible:{
+        color: 'transparent'
       },
     button: {
         borderRadius: 10,
